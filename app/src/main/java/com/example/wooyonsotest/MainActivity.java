9f31b094a11a9fa37d98816d7459c5f5a62b4a39
Lirodek <96456897+Lirodek@users.noreply.github.com>
2022-11-20 19:07:05 +0900

    fragment Change ver0.05
    fragment A => fragment B implementation

--- app/src/main/java/com/example/wooyonsotest/MainActivity.java
+++ app/src/main/java/com/example/wooyonsotest/MainActivity.java
@@ -7,61 +7,36 @@ import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.Toast;
 
 import com.example.wooyonsotest.fragment.FragmentView;
 
 public class MainActivity extends AppCompatActivity {
-    ImageButton ibCate1, ibHome;
+
     Button btnCate2, getBtnCate2;
     FragmentView fragment1;
-    private static Context context;
-    Button[] btnCate = new Button[2];
-    int[] btnCateId = {R.id.btnCate2, R.id.btnCate3};
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
-
-        ibCate1 = findViewById(R.id.ibCate1);
-        ibCate1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),1+"번 메뉴 선택",Toast.LENGTH_SHORT).show();
-            }
-        });
-        btnCateClickEvent();
-
-    }
-    public void btnCateClickEvent(){
-        for(int i=0; i<btnCateId.length; i++){
-            btnCate[i] = findViewById(btnCateId[i]);
-        }
-        for(int i=0; i<btnCateId.length; i++){
-            int finalI = i;
-            btnCate[i].setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Toast.makeText(getApplicationContext(),(finalI +1)+"번 메뉴 선택",Toast.LENGTH_SHORT).show();
-                }
-            });
-        }
-        this.context = getApplicationContext();
-        MainActivity.context = getApplicationContext();
 
         findViewById(R.id.mainLinear).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction(); // 주문내역으로 이동
                 ft.replace(R.id.frameLayout, new FragmentView());
                 ft.addToBackStack(null);
                 ft.commit();
-
             }
         });
+
+
+
     }
+
 
 
 
--- app/src/main/java/com/example/wooyonsotest/fragment/FragmentView.java
+++ app/src/main/java/com/example/wooyonsotest/fragment/FragmentView.java
@@ -5,11 +5,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -22,17 +26,45 @@ import java.util.ArrayList;
 
 //프래그먼트는 액티비티위에 올라가있을떄만 프래그먼트로서 동작할 수 있다.
 public class FragmentView extends Fragment {
-
+    ImageButton ibCate1, ibHome;
     LinearLayout layout;
     LinearLayout order_his;
     RecyclerView recyclerView;
     LinearLayoutManager linearLayoutManager;
-
+    Button[] btnCate = new Button[2];
+    int[] btnCateId = {R.id.btnCate2, R.id.btnCate3};
+    Context context;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.category, container, false);
-
+        View root = inflater.inflate(R.layout.category_main, container, false);
+        this.context = container.getContext();
+        ibCate1 = (ImageButton)root.findViewById(R.id.ibCate1);
+        ibCate1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context.getApplicationContext(),1+"번 메뉴 선택",Toast.LENGTH_SHORT).show();
+            }
+        });
+        btnCateClickEvent(root);
         return root;
+    }
+
+    public void btnCateClickEvent(View root){
+        for(int i=0; i<btnCateId.length; i++){
+            btnCate[i] = (Button) root.findViewById(btnCateId[i]);
+        }
+        for(int i=0; i<btnCateId.length; i++){
+            int finalI = i;
+            btnCate[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(context.getApplicationContext(),(finalI+2)+"번 메뉴 선택",Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+//        this.context = getApplicationContext();
+//        MainActivity.context = getApplicationContext();
+
     }
 }
